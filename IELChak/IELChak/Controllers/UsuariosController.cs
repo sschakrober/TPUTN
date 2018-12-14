039d221bd83d9d242f71e91a5af55c5a04d30d81
chakrober <sschakrober@gmail.com>
2018-12-14 01:00:56 -0300

    commit7-Datosdelusuario

--- IELChak/IELChak/Controllers/UsuariosController.cs
+++ IELChak/IELChak/Controllers/UsuariosController.cs
@@ -32,125 +32,41 @@ namespace IELChak.Controllers
             usuario.Add(appUsuario);
             return usuario;
         }
-
-        // GET: Usuarios/Details/5
-        public async Task<IActionResult> Details(string id)
+        public async Task<string> EditUsuario(string id, string userName, string email, string phoneNumber, int accessFailedCount, string concurrerncyStamp, bool emailConfirmed, bool lockoutEnabled, DateTimeOffset
+            lockoutEnd, string normalizedEmail, string normalizedUsedName, string passwordHash, bool phoneNumberConfirmed, string securityStamp, bool twoFactorEnabled, ApplicationUser applicationUser)
         {
-            if (id == null)
+            var resp = "";
+            try
             {
-                return NotFound();
-            }
+                applicationUser = new ApplicationUser
+                {
+                    Id = id,
+                    UserName = userName,
+                    Email = email,
+                    PhoneNumber = phoneNumber,
+                    EmailConfirmed = emailConfirmed,
+                    LockoutEnabled = lockoutEnabled,
+                    LockoutEnd = lockoutEnd,
+                    NormalizedEmail = normalizedEmail,
+                    NormalizedUserName = normalizedUsedName,
+                    PasswordHash = passwordHash,
+                    PhoneNumberConfirmed = phoneNumberConfirmed,
+                    SecurityStamp = securityStamp,
+                    TwoFactorEnabled = twoFactorEnabled,
+                    AccessFailedCount = accessFailedCount,
+                    ConcurrencyStamp = concurrerncyStamp,
+                };
 
-            var applicationUser = await _context.ApplicationUser
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (applicationUser == null)
-            {
-                return NotFound();
-            }
-
-            return View(applicationUser);
-        }
-
-        // GET: Usuarios/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Usuarios/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] ApplicationUser applicationUser)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(applicationUser);
+                _context.Update(applicationUser);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                resp = "Save";
             }
-            return View(applicationUser);
-        }
-
-        // GET: Usuarios/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
+            catch
             {
-                return NotFound();
+                resp = "No Save";
             }
 
-            var applicationUser = await _context.ApplicationUser.SingleOrDefaultAsync(m => m.Id == id);
-            if (applicationUser == null)
-            {
-                return NotFound();
-            }
-            return View(applicationUser);
-        }
-
-        // POST: Usuarios/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] ApplicationUser applicationUser)
-        {
-            if (id != applicationUser.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(applicationUser);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ApplicationUserExists(applicationUser.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(applicationUser);
-        }
-
-        // GET: Usuarios/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var applicationUser = await _context.ApplicationUser
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (applicationUser == null)
-            {
-                return NotFound();
-            }
-
-            return View(applicationUser);
-        }
-
-        // POST: Usuarios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var applicationUser = await _context.ApplicationUser.SingleOrDefaultAsync(m => m.Id == id);
-            _context.ApplicationUser.Remove(applicationUser);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return resp;
         }
 
         private bool ApplicationUserExists(string id)
--- IELChak/IELChak/wwwroot/js/site.js
+++ IELChak/IELChak/wwwroot/js/site.js
@@ -9,12 +9,30 @@ function getUsuario(id, action) {
         url: action,
         data: { id },
         success: function (response) {
-            console.log(response);
+            mostrarUsuario(response);
         }
     });
 }
 
 var items;
+
+var id;
+var userName;
+var email;
+var phoneNumber;
+var accessFailedCount;
+var concurrencyStamp;
+var emailConfirmed;
+var lockoutEnabled;
+var lockoutEnd;
+var normalizedUserName;
+var normalizedEmail;
+var passwordHash;
+var phoneNumberConfirmed;
+var securityStamp;
+var twoFactorEnabled;
+
+
 function mostrarUsuario(response) {
     items = response;
     $.each(items, function (index, val) {
@@ -22,5 +40,43 @@ function mostrarUsuario(response) {
         $('input[name=UserName]').val(val.userName);
         $('input[name=Email]').val(val.email);
         $('input[name=PhoneNumber]').val(val.phoneNumber);
+    });
+}
+
+function editarUsuario(action) {
+    id = $('input[name = Id]')[0].value;
+    email = $('input[name = Email]')[0].value;
+    phoneNumber = $('input[name = PhoneNumber]')[0].value;
+
+    $.each(items, function (index, val) {
+        accessFailedCount = val.accessFailedCount;
+        concurrencyStamp = val.concurrencyStamp;
+        emailConfirmed = val.emailConfirmed;
+        lockoutEnabled = val.lockoutEnabled;
+        lockoutEnd = val.lockoutEnd;
+        userName = val.userName;
+        normalizedUserName = val.normalizedUserName;
+        normalizedEmail = val.normalizedEmail;
+        passwordHash = val.passwordHash;
+        phoneNumberConfirmed = val.phoneNumberConfirmed;
+        securityStamp = val.securityStamp;
+        twoFactorEnabled = val.twoFactorEnabled;
+    });
+
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: {
+            id, userName, phoneNumber, accessFailedCount, concurrencyStamp, emailConfirmed, lockoutEnabled, lockoutEnd, normalizedEmail, normalizedUserName, passwordHash, phoneNumberConfirmed, securityStamp, twoFactorEnabled
+        },
+
+        success: function (response) {
+            if (response == "Save") {
+                window.location.href = "Usuarios";
+            }
+            else {
+                alert("No se puede editar los datos del usuario");
+            }
+        }
     });
 }
